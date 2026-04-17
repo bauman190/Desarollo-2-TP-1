3ab2d9782753c8ef4a6ce56fcdb7bd90d3f2410b
Bauman <baumancastignani@hotmail.com>
2026-04-17 17:52:17 -0300

    Bullets produce sound when fired

--- Assets/Scripts/Data/Bullet.cs
+++ Assets/Scripts/Data/Bullet.cs
@@ -1,14 +1,22 @@
 using UnityEngine;
 
 [RequireComponent(typeof(Rigidbody))]
+[RequireComponent(typeof(AudioSource))]
 public class Bullet : MonoBehaviour
 {
     Rigidbody rb;
+    [SerializeField] AudioSource AS;
+
     public void Awake()
     {
+        AS = GetComponent<AudioSource>();
         rb = GetComponent<Rigidbody>();
     }
 
+    private void Start()
+    {
+       AS.Play();
+    }
     public void Logic(float speed)
     {
         rb.AddForce(transform.forward * speed);
