0361bd3792738db0fcf7a29d16b2b4ba519fdfa5
unknown <baumancastignani@hotmail.com>
2026-04-20 12:44:52 -0300

    Asteroid Counter created

--- Assets/Scripts/Data/Spawner.cs
+++ Assets/Scripts/Data/Spawner.cs
@@ -5,6 +5,7 @@ public class Spawner : MonoBehaviour
     [SerializeField] private Asteroid prefabAsteroid;
     [SerializeField] private float coolDown = 10f;
     [SerializeField] private Transform player;
+    [SerializeField] private Transform asteroidCounter;
 
     private float timer = 0;
 
@@ -23,7 +24,7 @@ public class Spawner : MonoBehaviour
     }
     private void Spawn()
     {
-        Asteroid asteroid = Instantiate(prefabAsteroid, transform.position, Quaternion.identity);
+        Asteroid asteroid = Instantiate(prefabAsteroid, transform.position, Quaternion.identity, asteroidCounter);
         asteroid.SetTarget(player);
     }
 }
